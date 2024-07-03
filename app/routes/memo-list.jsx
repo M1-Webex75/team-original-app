2eb561a758125082ac53f27cdf063c251ce8ca60
yamasho1061 <uchiyamashota9@gmail.com>
2024-07-03 23:14:35 +0900

    プルダウンの機能完成、タイトルからから空ページへのリンクを作成、カレンダーページとメモ一覧ページの切り替え機能作成

--- app/routes/memo-list.jsx
+++ app/routes/memo-list.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { Link } from "@remix-run/react";
 import PullDown from "./pulldown";
+import { useNavigate } from "react-router-dom";
 
 const Table = ({ data }) => {
   return (
@@ -14,7 +16,9 @@ const Table = ({ data }) => {
       <tbody>
         {data.map((item) => (
           <tr key={item.title}>
-            <td>{item.title}</td>
+            <td>
+              <Link to={item.linkTo}>{item.title}</Link>
+            </td>
             <td>{item.date}</td>
             <td>{item.template}</td>
           </tr>
@@ -25,10 +29,30 @@ const Table = ({ data }) => {
 };
 
 const sampleData = [
-  { title: "○✕商事 ES", date: "2024/6/30", template: "就活" },
-  { title: "△△データ ES", date: "2024/7/10", template: "就活" },
-  { title: "ライブに行った", date: "2024/6/25", template: "日記" },
-  { title: "1日寝た", date: "2024/6/26", template: "日記" },
+  {
+    title: "○✕商事 ES",
+    date: "2024/6/30",
+    template: "就活",
+    linkTo: "/empty-page",
+  },
+  {
+    title: "△△データ ES",
+    date: "2024/7/10",
+    template: "就活",
+    linkTo: "/empty-page",
+  },
+  {
+    title: "ライブに行った",
+    date: "2024/6/25",
+    template: "日記",
+    linkTo: "/empty-page",
+  },
+  {
+    title: "1日寝た",
+    date: "2024/6/26",
+    template: "日記",
+    linkTo: "/empty-page",
+  },
 ];
 
 const makeDisplayData = (data, option) => {
@@ -42,9 +66,13 @@ const makeDisplayData = (data, option) => {
 export default function Index() {
   const [selectedOption, setSelectedOption] = useState("");
 
+  const navigate = useNavigate();
+
   return (
     <>
-      <h1>メモリストのページです</h1>
+      <h1>メモ一覧</h1>
+      <button onClick={() => navigate("/calender")}>Calender</button>
+      <button>Memo List</button>
       <div>
         <PullDown
           selectedOption={selectedOption}

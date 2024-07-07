fb32d4d0a229f7f16d81c1612c9ef638571dbfb7
yamasho1061 <uchiyamashota9@gmail.com>
2024-07-07 12:12:50 +0900

    新規作成ボタンを追加、PULLDOWNを一部修正

--- app/routes/pulldown.jsx
+++ app/routes/pulldown.jsx
@@ -1,19 +1,18 @@
 import { useState, useRef } from "react";
 
-const PullDown = ({ selectedOption, setSelectedOption }) => {
+const PullDown = ({ labelText = "", selectedOption, setSelectedOption }) => {
   const handleChange = (e) => {
     setSelectedOption(e.target.value);
   };
 
   return (
     <div>
-      <label htmlFor="options">テンプレートでフィルター:</label>
+      <label htmlFor="options">{labelText}</label>
       <select id="options" value={selectedOption} onChange={handleChange}>
         <option value="">未選択</option>
         <option value="就活">就活</option>
         <option value="日記">日記</option>
       </select>
-      <p>Selected option: {selectedOption}</p>
 
       <table></table>
     </div>
